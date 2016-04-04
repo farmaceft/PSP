454ba2e4d6d768907950ca4bc35559a42f353783
Fox <codezfox@live.ru>
2016-04-04 22:33:47 +0300

    add User and Role models, and create database

---
+++ WebApplicationLab/Models/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace WebApplicationLab.Models
+{
+    public class HelpDeskContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApplicationLab/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationLab.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Логин")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Login { get; set; }
+        
+        [Required]
+        [Display(Name = "Пароль")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Password { get; set; }
+        
+        [Required]
+        [Display(Name = "Статус")]
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
