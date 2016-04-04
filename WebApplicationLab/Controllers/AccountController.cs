61b9f1c916bcad80b303ebbd54908895ed058bf9
Fox <codezfox@live.ru>
2016-04-05 00:52:09 +0300

    add AccountController, action "Log in"

---
+++ WebApplicationLab/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplicationLab.Models;
+
+namespace WebApplicationLab.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LogViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный пароль или логин");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login", "Account");
+        }
+
+        private bool ValidateUser(string login, string password)
+        {
+            bool isValid = false;
+
+            using (HelpDeskContext db = new HelpDeskContext())
+            {
+                try
+                {
+                    User user = (from u in db.Users
+                                 where u.Login == login && u.Password == password
+                                 select u).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        isValid = true;
+                    }
+                }
+                catch
+                {
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+    }
+}
