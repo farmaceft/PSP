ab85afc6dc12e47aac1b1ec12f5555f7bf22e415
Fox <codezfox@live.ru>
2016-04-05 09:23:39 +0300

    add UserController with users list (Index)

---
+++ WebApplicationLab/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using WebApplicationLab.Models;
+
+namespace WebApplicationLab.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class UsersController : Controller
+    {
+        private HelpDeskContext db = new HelpDeskContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Role).ToList();
+            return View(users);
+        }
+	}
+}
