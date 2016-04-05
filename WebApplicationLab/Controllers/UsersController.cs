1e557c9b5b8096c3517ac7e3a16a0e074390dd80
Fox <codezfox@live.ru>
2016-04-05 20:22:41 +0300

    add action "Edit user"

--- WebApplicationLab/Controllers/UsersController.cs
+++ WebApplicationLab/Controllers/UsersController.cs
@@ -16,5 +16,44 @@ namespace WebApplicationLab.Controllers
             var users = db.Users.Include(u => u.Role).ToList();
             return View(users);
         }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int id)
+        {
+            User user = db.Users.Find(id);
+            //SelectList departments = new SelectList(db.Departments, "Id", "Name", user.DepartmentId);
+            //ViewBag.Departments = departments;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name", user.RoleId);
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            SelectList roles = new SelectList(db.Roles, "id", "Name");
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 	}
 }
