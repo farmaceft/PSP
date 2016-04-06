2a78c2e3b795a96a053b3e987f732ec66bf2850e
Fox <codezfox@live.ru>
2016-04-07 02:00:59 +0300

    added user registration

--- WebApplicationLab/Controllers/AccountController.cs
+++ WebApplicationLab/Controllers/AccountController.cs
@@ -8,6 +8,37 @@ namespace WebApplicationLab.Controllers
     [AllowAnonymous]
     public class AccountController : Controller
     {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ValidateLoginUser(model.UserName))
+                {
+                    CreateUser(model.UserName, model.Password);
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с данным логином уже существует");
+                }
+            }
+            return View(model);
+        }
+
         public ActionResult Login()
         {
             return View();
@@ -43,6 +74,46 @@ namespace WebApplicationLab.Controllers
             FormsAuthentication.SignOut();
 
             return RedirectToAction("Login", "Account");
+        }
+
+        private void CreateUser(string login, string password)
+        {
+            using (HelpDeskContext db = new HelpDeskContext())
+            {
+                try
+                {
+                    db.Users.Add(new User() {Login = login, Password = password, RoleId = 2});
+                    db.SaveChanges();
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+        }
+        private bool ValidateLoginUser(string login)
+        {
+            bool isValid = true;
+
+            using (HelpDeskContext db = new HelpDeskContext())
+            {
+                try
+                {
+                    User user = (from u in db.Users
+                                 where u.Login == login
+                                 select u).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        isValid = false;
+                    }
+                }
+                catch
+                {
+                    isValid = true;
+                }
+            }
+            return isValid;
         }
 
         private bool ValidateUser(string login, string password)
---
+++ WebApplicationLab/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationLab.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords don't match.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string PasswordConfirm { get; set; }
+
+        [Required]
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+    }
+}
